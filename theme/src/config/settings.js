1e6ec998f0482d8ac41d2581b689fb6b5a7e8a99
Laura <laurakbrandon@gmail.com>
2022-08-27 00:46:30 +1000

    update home hero

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Laura Brandon`,
+	description: `A portfolio of my personal projects, snippets of code, mechanical keyboard builds, and occasional corgi pics.`,
+	url: `https://laurabrandon.codes`, // No trailing slash!
+	name: `Laura Brandon`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 };
