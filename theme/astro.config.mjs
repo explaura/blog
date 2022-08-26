9a34326431e2853a8668c1ac319841365be8d773
Laura <laurakbrandon@gmail.com>
2022-08-27 00:09:51 +1000

    change site name

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://odyssey-theme.littlesticks.dev/',
+	site: 'https://laurabrandon.codes/',
 	// Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
 	sitemap: true,
 	// Generate sitemap (set to "false" to disable)
