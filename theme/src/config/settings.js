231649bf14b508d9053017d7ce9776e1b18b0a63
Laura <laurakbrandon@gmail.com>
2022-08-31 21:30:55 +1000

    disable theme switcher

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -3,5 +3,5 @@ export default {
 	description: `A portfolio of side projects, snippets of code, mechanical keyboard builds, and occasional corgi pics.`,
 	url: `https://laurabrandon.codes`, // No trailing slash!
 	name: `Laura Brandon`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
+	enableThemeSwitcher: false,
 };
